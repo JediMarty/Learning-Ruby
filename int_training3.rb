a1ca931d0325bad98ab3b40e41557b906712c99e
JediMarty <130549056+JediMarty@users.noreply.github.com>
2023-05-14 14:03:32 +0300

    Add files via upload

---
+++ int_training3.rb
@@ -0,0 +1,9 @@
+puts "Please enter float number: "
+fnum = gets.chomp().to_f
+puts "Please enter another float number"
+fnum2 = gets.chomp().to_f
+answer = fnum + fnum2
+puts ("The answer is: " + answer.round().to_s)
+
+
+
