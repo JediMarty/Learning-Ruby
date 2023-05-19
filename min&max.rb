a3f3f599ee88185d2c002fbebe82be4b58cb1cdf
JediMarty <130549056+JediMarty@users.noreply.github.com>
2023-05-19 10:03:08 +0300

    Update min&max.rb

--- min&max.rb
+++ min&max.rb
@@ -1,9 +1,9 @@
 def mm(num1,num2)
     if num1 >= num2
-return num1
+return ("Max number -> #{num1}\nMin number -> #{num2}")
     
 else 
-    return num2
+    return ("Max number -> #{num2}\nMin number -> #{num1}")
 end
 
 end
