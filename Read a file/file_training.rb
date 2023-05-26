6927f7c2ae2d06adf46191777398c0ce2fd9f813
JediMarty <130549056+JediMarty@users.noreply.github.com>
2023-05-26 18:14:39 +0300

    Update file_training.rb

--- Read a file/file_training.rb
+++ Read a file/file_training.rb
@@ -1,3 +1,9 @@
-file = File.opne(employees.txt,r)
+file = File.open("c:/Users/Admin/Desktop/Read a file/employees.txt","r")
+   
+for line in file.readlines()
+    if line.include?("Male")
+    puts line
+    end
+end
 
-file.close()
+   file.close()
