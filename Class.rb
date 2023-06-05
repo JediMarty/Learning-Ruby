5afd51edf3d2d54b387d91c8ee9509820b97c292
JediMarty <130549056+JediMarty@users.noreply.github.com>
2023-06-05 17:58:24 +0300

    Update Class.rb

--- Class.rb
+++ Class.rb
@@ -1,17 +1,15 @@
 class Book
     attr_accessor :title, :author, :pages
-  
+  def initialize(title,author,pages)
+     @title = title
+     @author = author
+     @pages = pages
+  end
 end
 
-book1 = Book.new()
-book1.title = "Forestmen"
-book1.author = "JediMarty"
-book1.pages = 120
+book1 = Book.new("Forestmen","JediMarty",120)
 
-book2 = Book.new()
-book2.title = "The lost treasure"
-book2.author = "JediMarty"
-book2.pages = 320
+book2 = Book.new("The lost treasure","JediMarty",320)
 
 puts book1.author
 puts book2.title
