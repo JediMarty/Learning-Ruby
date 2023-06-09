33fa6fc42a6ab71053fd34cce5f086af891b92fc
JediMarty <130549056+JediMarty@users.noreply.github.com>
2023-06-09 19:21:06 +0300

    Update pokemons.rb

--- pokemons.rb
+++ pokemons.rb
@@ -6,9 +6,19 @@ class Pokemon
         @ability = ability
         @power = power
     end
-
+       
+    def >(other)
+        if self.power > other.power
+            puts "You hit with #{self.ability}"
+        else
+            puts "You got DAMAGE from #{other.ability}"
+        end
+    end
+    
 end
 
-creature1 = Pokemon.new("Hitmontop","Cyclone Kick", 100)
-creature2 = Pokemon.new("Charmander","Flare", 30)
+#Creating two pokemons!
+creature1=Pokemon.new("Hitmontop","Cyclone Kick", 100) 
+creature2=Pokemon.new("Charmander","Flare", 30)
 
+creature1>creature2
