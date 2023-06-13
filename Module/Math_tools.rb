73a5b5ab4d2ca9c0e3bb4e1050ac5171c44e0600
JediMarty <130549056+JediMarty@users.noreply.github.com>
2023-06-13 18:38:54 +0300

    Add files via upload

---
+++ Module/Math_tools.rb
@@ -0,0 +1,15 @@
+module MTools
+
+    def pow(x,y)
+        print x**y
+    end
+
+    def arr_greater(arr)
+        arr.each do |idx|
+           if idx > 5
+             puts idx
+            end
+        end
+    end
+
+end
